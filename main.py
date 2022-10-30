c8803f6150d98deb653c4552e5c466184400cc11
Kesheon <treymccullough17@gmail.com>
2022-10-30 08:43:39 -0500

    added register view

--- main.py
+++ main.py
@@ -13,5 +13,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
